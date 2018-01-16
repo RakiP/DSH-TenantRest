874201138f69c6a09ba17409665af1ca18e46fb7
Partapsing, Rakesh <rakesh.partapsing@kpn.com>
2018-01-16 09:32:34 +0100

    added templates for pipeline xml's

--- dockerKJ/py/TenantRestConfig.py
+++ dockerKJ/py/TenantRestConfig.py
@@ -6,8 +6,8 @@ home = expanduser("~")
 absFilePath = os.path.dirname(os.path.abspath(__file__))
 dMountedDiskClient = os.path.join(absFilePath,'mountedDisk')
 fKafkaRunCommand = os.path.join(absFilePath,'kafkaProduceRunCommand.py')
-templatePushMDisk =  os.path.join(absFilePath,'templates','push-mountedDisk.xml')
-templateExecKafkaRunCommand = os.path.join(absFilePath,'templates','executeProduceRunCommand.xml')
+templatePushMDisk =  os.path.join(absFilePath,'templates','push-mountedDisk-pipeline.xml')
+templateExecKafkaRunCommand = os.path.join(absFilePath,'templates','executeProduceRunCommand-pipeline.xml')
 
 portJenkins = 8084
 portKafka = 9092
